5a5ac6e8a12d2d6ab06c8016e3b03aafad79feba
ruansongsong <ruansongsong@gmail.com>
2017-02-16 08:35:13 +0800

    add some notes

---
+++ test/test.js
@@ -0,0 +1,46 @@
+// function a() {
+// 	var b = 10;
+// }
+// a();
+// console.log(b);
+// if(true) {
+// 	var a = 10;
+// }
+// console.log(a);
+//
+// var d = new Date();
+//
+// console.log(d.toLocaleString()); //"04.09.2016, 15:42:44"
+// console.log(d.toString());
+// var a = [1, 2, 3];
+// console.log(a.join('-'));
+
+// var a = new Date();
+// console.log(a);
+// console.log(Date.now());
+// console.log(a.getFullYear());
+// var a = 'abc'.replace(/b/g, "233")
+// console.log(a);
+// var a = 'erver'.match(/er/);
+// console.log(a); // [ 'er', index: 0, input: 'erver' ]
+// var b = 'erver'.match(/er/g);
+// console.log(b); // [ 'er', 'er' ]
+// var c = 'abcbdbr'.split(/x/);
+// console.log(c); // [ 'a', 'c', 'd', 'r' ]
+// var reg = /b/g;
+// console.log(reg.exec('abcbyybh')); // [ 'b', index: 1, input: 'abcbyybh' ]
+// console.log(reg.exec('abcbyybh')); // [ 'b', index: 3, input: 'abcbyybh'
+// console.log(reg.exec('abcbyybh')); // [ 'b', index: 6, input: 'abcbyybh' ]
+// // console.log(reg.exec('abcbyybh')); // null
+// var a = /a+/;
+// var b = /a?/
+// var str = 'aaab';
+// console.log(str.match(a));
+// console.log(str.match(b));
+// var reg = /\b(?:[01]?[0-9]|2[0-3]):[0-5][0-9]\b/;
+// console.log('现在是09:49点'.match(reg));	// ["09:49"]
+// var a = 's3e#%rw';
+// console.log(escape(a));
+// var a = Math.floor(Math.random() * 9 + 2);
+// console.log(a)
+console.log(Date.now());
---
+++ 前端/DOM相关.md
@@ -0,0 +1,3 @@
+# DOM相关知识点
+## `DOMContentLoaded`和`load`事件区别
+先触发`DOMContentLoaded`事件，再触发`load`事件
